89ceb9c9a7a9c817aaeb184866502c4b255fb05e
sparkle666 <sikky606@gmail.com>
2021-10-08 10:00:34 +0100

    more files

---
+++ Mamah_Emmanuel.py
@@ -0,0 +1,5 @@
+name = "Mamah Emmanuel Nnadozie"
+greet = "Hello world"
+
+print(name)
+print(greet)    
---
+++ Ukachukwu_Chibuzor.py
@@ -0,0 +1,3 @@
+name = "My Name Is Ukachukwu Chibuzor"
+greet = "Hello World"
+print(greet, "\n", name)
